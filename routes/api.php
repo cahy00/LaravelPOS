1828caf2ecd6917b00cda498d9e839125a2ee43d
cahy00 <cariercahyo@gmail.com>
2022-06-30 09:26:33 +0800

    #

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\Api\TodoController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {

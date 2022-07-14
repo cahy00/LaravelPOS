61b6ddbaa2310b1645f4958b2d841cdb144f2788
cahy00 <cariercahyo@gmail.com>
2022-07-14 10:23:28 +0800

    #

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -25,7 +25,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->jobTitle,
-						// 'slug' =>	Str::slug('name')sadsds
+						// 'slug' =>	Str::slug('name')
         ];
     }
 }

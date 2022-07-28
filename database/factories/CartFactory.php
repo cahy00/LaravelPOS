507d32c0378af2201ec45da1353744692fe8ff14
cahy00 <cariercahyo@gmail.com>
2022-07-28 09:09:24 +0800

    #

--- database/factories/CartFactory.php
+++ database/factories/CartFactory.php
@@ -15,7 +15,7 @@ class CartFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition()DS
     {
 
         return [

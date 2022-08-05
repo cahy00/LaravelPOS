a283d601f8e2548ff5d9d8ddc048a566359f6a50
cahy00 <cariercahyo@gmail.com>
2022-08-05 10:57:08 +0800

    #

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
         return [
             'name' 		 => $this->faker->name(),
             'email'    => $this->faker->unique()->safeEmail(),
-            'password' => Hash::make('password'),fddsdf
+            'password' => Hash::make('password'),fddsdfsd
 						'role'     => 'admin'
         ];
     }
